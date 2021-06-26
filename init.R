58dd83baa3b5fae1df179c3eb96b9a5fcc0be53e
Aman Kumar <69099346+SHADOW-AJ@users.noreply.github.com>
2021-06-26 22:38:01 +0530

    Update init.R

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-my_packages = c("leaflet", "readr","rjson","ggplot2","rgdal")
+my_packages = c("leaflet", "readr","rjson","ggplot2")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
@@ -7,3 +7,4 @@ install_if_missing = function(p) {
 }
 
 invisible(sapply(my_packages, install_if_missing))
+install.packages("/app/localpkgs/Rnlminb2_2110.79.tar.gz", repos=NULL, type="source")
